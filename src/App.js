1b5bf4e76ba5f0a28b79679551f01ee9dfccc7e8
Rory Mapstone <rory.mapstone@gmail.com>
2018-09-07 11:58:21 +0200

    Sort of fixes login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -19,13 +20,11 @@ class App extends Component {
     firebase.initializeApp(config);
   }
   render() {
-    const store = createStore(reducers);
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
     return (
       <Provider store={store}>
-        <View>
-          <Text>Welcome to React Native!</Text>
-        </View>
+        <LoginForm/>
       </Provider>
     );
   }
